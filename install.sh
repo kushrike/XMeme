02d0b6c0c7612049e37aecf0152c111fd10d7dcf
Kushal <kushrike@gmail.com>
2021-02-10 21:53:56 +0530

    Modified

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 sudo apt update -y
 sudo apt-get install -y mongodb
+curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y npm
-
+cd backend; sudo npm install pm2 -g
