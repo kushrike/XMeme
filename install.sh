6ba555db3c85791e5824abca9d903afcf38adfc3
Ubuntu <ubuntu@ip-172-31-21-74.us-east-2.compute.internal>
2021-02-10 16:20:17 +0000

    Added PM2

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ sudo apt-get install -y mongodb
 curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y npm
-
+cd backend; sudo npm install pm2 -g
 
