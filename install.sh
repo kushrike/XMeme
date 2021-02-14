8a6b5769d25170c2d8220921865ce6889dff9180
Kushal <kushrike@gmail.com>
2021-02-14 21:24:53 +0530

    Modified DOCKERFILE to use Ubuntu 18.04 image

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt-get install -y mongodb
 sudo systemctl start mongodb.service
-curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
+sudo apt-get install -y curl
+curl -fsSL https://deb.nodesource.com/setup_15.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y npm
 cd backend; sudo npm install pm2 -g
